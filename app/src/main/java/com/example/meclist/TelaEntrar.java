983298040017e15db7ab3d3800d3773dd9860bdd
Gabriel Brandl <gabrielpaivabarbosa@gmail.com>
2024-04-24 18:51:10 -0300

    Tela de Entrar

---
+++ app/src/main/java/com/example/meclist/TelaEntrar.java
@@ -0,0 +1,16 @@
+package com.example.meclist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+class TelaEntrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_entrar);
+
+    }
+}
