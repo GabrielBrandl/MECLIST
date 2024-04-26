242b7144baf8003eed83cf11e20ff5e2829c70fb
Gabriel Brandl <gabrielpaivabarbosa@gmail.com>
2024-04-26 02:03:39 -0300

    Criando a TelaEntrar e aplicando a função no botão "Entrar" da primeira Tela do app

--- app/src/main/java/com/example/meclist/MainActivity.java
+++ app/src/main/java/com/example/meclist/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.meclist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button acessar;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        acessar = (Button) findViewById(R.id.button_tela_entrar_entrar);
+
+        acessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivities(new Intent[]{new Intent(MainActivity.this, TelaEntrar.class)});
+            }
+        });
     }
-}
+};
--- app/src/main/java/com/example/meclist/TelaEntrar.java
+++ app/src/main/java/com/example/meclist/TelaEntrar.java
@@ -1,16 +1,14 @@
 package com.example.meclist;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-class TelaEntrar extends AppCompatActivity {
+public class TelaEntrar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_entrar);
-
     }
 }
