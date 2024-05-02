8ae1949564c5f3d382a1da4d3f28d75d0d5f5502
Gabriel Brandl <129007050+GabrielBrandl@users.noreply.github.com>
2024-05-02 18:09:29 -0400

    Criando a tela de Inserir Código, e configurando os botões da tela EsqueciSenha

--- app/src/main/java/com/example/meclist/EsqueciSenha.java
+++ app/src/main/java/com/example/meclist/EsqueciSenha.java
@@ -13,11 +13,13 @@ public class EsqueciSenha extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         Button voltar;
+        Button enviar;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esqueci_senha);
 
         voltar = (Button) findViewById(R.id.button_voltar_tela_entrar);
+        enviar = (Button) findViewById(R.id.button_tela_entrar_enviar);
 
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +27,14 @@ public class EsqueciSenha extends AppCompatActivity {
                 startActivities(new Intent[]{new Intent(EsqueciSenha.this, TelaEntrar.class)});
             }
         });
+
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivities(new Intent[]{new Intent(EsqueciSenha.this, TelaInserirCodigo.class)});
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/meclist/TelaInserirCodigo.java
+++ app/src/main/java/com/example/meclist/TelaInserirCodigo.java
@@ -2,13 +2,28 @@ package com.example.meclist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaInserirCodigo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button voltar_inserir;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inserir_codigo);
+
+        voltar_inserir = (Button) findViewById(R.id.button_voltar_esqueci_senha);
+
+        voltar_inserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivities(new Intent[]{new Intent(TelaInserirCodigo.this, EsqueciSenha.class)});
+            }
+        });
     }
 }
